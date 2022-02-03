63d3af416cbd95f07c40ef39f106df03785ac45c
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-02-03 00:24:28 -0300

    Mudança de gradiente

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,7 +4,7 @@ const Header: React.FC = () => {
   return (
     <div className="flex items-center border-b-2 border-gray-200 px-6 p-3">
       <ClipboardListIcon className="h-7 w-7 mr-2" />
-      <span className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-pink-600 to-blue-600">Task List</span>
+      <span className="text-2xl font-bold text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-blue-600">Task List</span>
     </div>
   )
 }
--- components/tasks/Task.tsx
+++ components/tasks/Task.tsx
@@ -13,7 +13,7 @@ const Task: React.FC<TaskProps> = ({ task }) => {
     <article className="flex items-center justify-between py-3 px-6 hover:bg-gray-50">
       <div>
         <h2 className="font-semibold text-slate-900">{task.title}</h2>
-        <h6 className="text-xs font-medium text-transparent bg-clip-text bg-gradient-to-br from-pink-600 to-blue-600">{new Date(task.due_date).toLocaleDateString()}</h6>
+        <h6 className="text-xs font-medium text-transparent bg-clip-text bg-gradient-to-br from-pink-500 to-blue-600">{new Date(task.due_date).toLocaleDateString()}</h6>
       </div>
       <Toggle color='green-400' />
     </article>
